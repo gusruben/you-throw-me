1721293c3ddb1bbe252681dc0bbb0294c9fb7ff5
Sebastian H. <maracuchoamericano@gmail.com>
2025-03-01 17:15:29 -0500

    hg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,32 +33,35 @@
 
 
     // amountOfRectangles reprsents the interval of each datapoint
-    function LRAM(list, totalTime) {
-        
-        let totalSum = 0;
-        let xVelocityData = [];
-        for (let i=0; i<list.length; i++) {
-            totalSum += list[i];
-            // amount of rectangles = b-a = finalTime-initialTime (ms)
-            xVelocityData.push((((totalTime*0.001))/(list.length)) * (list[i]));
-        }
+    function LRAM(listX, listY, interval) {
+    let velocityData = [];
+    let velocityX = 0;
+    let velocityY =0;
 
-        xVelocity = (list.length/totalTime) * (totalSum);
-        return xVelocityData;
+    for (let i = 0; i < listX.length; i++) {
+        const accelX = Math.abs(listX[i]) > 0.3 ? listX[i] : 0;
+        const accelY = Math.abs(listY[i]) > 0.3 ? listY[i] : 0;
+        velocityX += accelX * (interval / 1000);
+        velocityY += accelY * (interval / 1000);
+        
+        // Push the velocity magnitude
+        velocityData.push(Math.sqrt(velocityX ** 2 + velocityY ** 2));
     }
 
+    
+    return velocityData;
+}
 
-    function getDistance(list, totalTime) {
-        divider = (totalTime*0.001)/list.length;
-        
-        let totalSum = 0;
-        for (let i=0; i<list.length; i++) {
-            totalSum += list[i];
-        }
+function getDistance(list, interval) {
+    let totalDistance = 0;
 
-
-        return ((totalTime*0.001)/(list.length*10)) * (totalSum);
+    for (let i = 0; i < list.length; i++) {
+        totalDistance += list[i] * (interval / 1000); // Convert ms to seconds
     }
+
+    return totalDistance;
+}
+
 
 
     onMount(async() => {
@@ -66,43 +69,46 @@
         // Assume on the ground when z=9.8 or so
     
         window.addEventListener("devicemotion", (event) => {
-            interval = event.interval;
-            x = event.accelerationIncludingGravity.x;
-            y = event.accelerationIncludingGravity.y;
+    interval = event.interval; // Time step in ms
+    x = event.accelerationIncludingGravity.x;
+    y = event.accelerationIncludingGravity.y;
 
-            xData.push(x);
-            yData.push(y);
+    if (Math.abs(x) > 0.3 || Math.abs(y) > 0.3) {
+        xData.push(x);
+        yData.push(y);
+    }
+    
+    
 
-            z = event.accelerationIncludingGravity.z-9.8;
-            
+    z = event.accelerationIncludingGravity.z - 9.8;
 
-            const marginOfError = 0.2;
+    const marginOfError = 0.2;
 
-            if ((marginCheck > 4) && !isOnGround) {
-                isOnGround = true;
-                timeElapsed = Date.now() - initialTime;
-                
-                const xVelocityData = LRAM(xData, timeElapsed);
+    if ((marginCheck > 4) && !isOnGround) {
+        isOnGround = true;
+        timeElapsed = Date.now() - initialTime;
+        
+        
 
-                totalDisplacement = getDistance(xVelocityData, timeElapsed);
-                if (totalDisplacement > highestDisplacement) {
-                    highestDisplacement = totalDisplacement;
-                }
-                xData = [];
-                yData = [];
-                initialTime = Date.now();
+        const velocityData = LRAM(xData, yData, interval);
 
-            }
+        totalDisplacement = Math.abs(getDistance(velocityData, interval));
 
+        if (totalDisplacement > highestDisplacement) {
+            highestDisplacement = totalDisplacement;
+        }
+        xData = [];
+        yData = [];
 
-            if (Math.abs(z) < marginOfError) {
-                marginCheck++;
-            } else {
-                marginCheck = 0; 
-                isOnGround=false;
-            }
+    }
 
-        });
+    if (Math.abs(z) < marginOfError) {
+        marginCheck++;
+    } else {
+        marginCheck = 0;
+        isOnGround = false;
+    }
+});
 
 
 
@@ -113,10 +119,10 @@
 
 
     const startTimer = () => {
-        setTimeout(() => {  
-            makeScreenRed = true;
-            initialTime = Date.now();
-        }, 3000)
+        xData = [];
+        yData = [];
+        initialTime = Date.now();
+        highestDisplacement = 0;
 
 
     }
@@ -148,6 +154,6 @@
 
 <div class="p-4 bg-blue-300 w-fit hover:bg-blue-400" onclick={startTimer}>
 
-    start timer
+    reset values
 </div>
 
