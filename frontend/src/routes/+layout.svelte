dbd489767ba003950a4c48a14486b60fd57b45f3
Gus Ruben <gusruben@protonmail.com>
2025-03-05 17:51:39 -0500

    Add metadata

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 {@render children()}
 
 <svelte:head>
+	<meta property="og:title" content="YOU THROW ME">
+	<meta property="og:description" content="A mobile game about destroying expensive technology ">
+	<meta property="og:image" content="/logoanim.gif">
+	<meta property="og:url" content="https://youthrow.me">
+	<meta property="og:type" content="website">	
+
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
